57bd4b80905b6976f6b6164c83c88f03ba2be388
geniusidea_coworking <kukuhpbd09@gmail.com>
2021-03-08 15:51:56 +0700

    first commit

---
+++ app/src/main/java/com/saspossible/myapplication/ItemsExample.java
@@ -0,0 +1,9 @@
+package com.saspossible.myapplication;
+
+import android.util.Log;
+
+public class ItemsExample {
+    public void printLog(String msg){
+        Log.d("ItemsExample", msg);
+    }
+}
